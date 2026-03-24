5eb00a4c5b9a2572f1569c7f98b30aaf81a9f1ab
Uami-11 <nirwan.m.9618@gmail.com>
2026-03-24 19:08:41 +0545

    added a moveable character

--- main.go
+++ main.go
@@ -1,30 +1,64 @@
 package main
 
 import (
+	"image"
+	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
+type Game struct {
+	// including player image into the game
+	PlayerImage *ebiten.Image
+	X           float64
+	Y           float64
+}
 
 func (g *Game) Update() error {
+	if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		g.X += 2
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		g.X -= 2
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyUp) {
+		g.Y -= 2
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyDown) {
+		g.Y += 2
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	screen.Fill(color.RGBA{120, 180, 255, 255})
+
+	opts := &ebiten.DrawImageOptions{}
+	opts.GeoM.Translate(g.X, g.Y)
+	// drawing our player
+	screen.DrawImage(g.PlayerImage.SubImage(image.Rect(0, 0, 16, 16)).(*ebiten.Image), opts)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+	return 320, 200
 }
 
 func main() {
-	ebiten.SetWindowSize(1280, 800)
+	ebiten.SetWindowSize(640, 400)
 	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	// ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+
+	// NewImageFromFile return *ebiten.Image, then go's image.Image, and then an error
+	// we use the ebiten image to actually render the thing, image.Image is the image data, and then error if not found
+	PlayerImg, _, err := ebitenutil.NewImageFromFile("assets/images/characters/demon.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// in the run game we insert the player image we got from new iamge from file into the actual RunGame struct
+	if err := ebiten.RunGame(&Game{PlayerImage: PlayerImg, X: 160, Y: 100}); err != nil {
 		log.Fatal(err)
 	}
 }
